0ecf734fc459258d181906d292e1d8c26972fcc8
Irene Wang <ireneziwang@gmail.com>
2024-03-15 17:25:45 -0700

    make CommentInput component

--- src/CommentSection.tsx
+++ src/CommentSection.tsx
@@ -1,8 +1,7 @@
 import { React, FC } from 'react';
 import Comment from './Comment.tsx';
+import CommentInput from './CommentInput.tsx';
 
-
-import { PaperAirplaneIcon } from '@heroicons/react/20/solid'
 
 type CommentProps = {
   id: number;
@@ -32,27 +31,11 @@ const CommentSection: FC<CommentProps> = ({ comments }) => {
 
           ))
         }
-        <div className="flex py-2">
-          <label htmlFor="comment" className="self-center block pl-3 py-2 text-l font-medium leading-6 text-gray-900">
-            <div className="avatar placeholder">
-              <div className="bg-neutral text-neutral-content rounded-full w-8">
-                <span className="text-sm">Me</span>
-              </div>
-            </div>
-      </label>
-      <div className="flex pl-2 self-center w-full relative">
-        <input
-          type="text"
-          name="comment"
-          id="comment"
-          className="block w-full bg-gray-200 rounded-full border-0 pl-4 pr-10 py-1.5 text-gray-900 sm:text-sm sm:leading-6 focus:ring-1 focus:ring-inset focus:ring-accent"
-          placeholder="Write a comment..."
-        />
-        <PaperAirplaneIcon className="absolute right-4 top-1/2 transform -translate-y-1/2 h-6 w-6 text-gray-500" />
-      </div>
+      <CommentInput
 
-    </div>
+        />
       </div >
+
 
     </div >
   );
